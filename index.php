f39a3f8e7ddc5a950779585a04ba3336eef31509
zkhb9m957z-byte <zkhb9m957z@privaterelay.appleid.com>
2026-03-11 13:37:57 +0100

    ik heb een takelijst aangemaakt

--- index.php
+++ index.php
@@ -2,13 +2,46 @@
 <html lang="nl">
 
 <head>
-    <title></title>
+    <title>TakenLijst</title>
     <?php require_once 'head.php'; ?>
 </head>
 
 <body>
+
+ <button id="addTask">Nieuwe Taak Toevoegen</button>
     
     <div class="container">
+  
+    <div class="form-group">
+        <label for="task">To Do:</label>
+        <div>
+
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="progress">Progress:</label>
+        <input type="text" class="form-control" id="progress" placeholder="Voeg de voortgang van de taak toe...">
+    </div>
+
+
+
+    <div class="form-group">
+        <label for="Review">Review:</label>
+        <input type="text" class="form-control" id="Review" placeholder="Voeg de review van de taak toe...">
+
+    </div>
+
+
+    <div class="form-group">
+        <label for="Done">Done:</label>
+        <input type="text" class="form-control" id="Done" placeholder="Voeg de status van de taak toe...">
+    </div>
+
+
+    <div></div>
+
+
 
 
     </div>
